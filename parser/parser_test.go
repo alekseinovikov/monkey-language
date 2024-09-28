2a4afa226ac852a33a77f037802c232bdad00987
alekseinovikov <novikov.aleksei.92@gmail.com>
2024-09-28 22:21:34 +0300

    Writing parser

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -15,6 +15,7 @@ func TestLetStatements(t *testing.T) {
 	l := lexer.New(input)
 	p := New(l)
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
 
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
@@ -28,7 +29,8 @@ func TestLetStatements(t *testing.T) {
 		expectedIdentifier string
 	}{
 		{"x"},
-		{"y"}, {"foobar"},
+		{"y"},
+		{"foobar"},
 	}
 	for i, tt := range tests {
 		stmt := program.Statements[i]
@@ -42,18 +44,34 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
 		return false
 	}
+
 	letStmt, ok := s.(*ast.LetStatement)
 	if !ok {
 		t.Errorf("s not *ast.LetStatement. got=%T", s)
 		return false
 	}
+
 	if letStmt.Name.Value != name {
 		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
 		return false
 	}
+
 	if letStmt.Name.TokenLiteral() != name {
 		t.Errorf("s.Name not '%s'. got=%s", name, letStmt.Name)
 		return false
 	}
+
 	return true
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
 }
